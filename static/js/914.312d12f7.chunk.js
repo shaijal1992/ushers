b56e5e0f92cdd286c1ca4e7170fcf505d1c18301
shaijal1992 <150117208+shaijal1992@users.noreply.github.com>
2023-11-07 10:16:28 +0300

    Add files via upload

---
+++ static/js/914.312d12f7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[914],{7914:function(e,n,a){a.r(n);a(2791);var r=a(3329);n.default=function(){return(0,r.jsx)("div",{children:(0,r.jsx)("h1",{children:"Dashboard !"})})}}}]);
+//# sourceMappingURL=914.312d12f7.chunk.js.map
